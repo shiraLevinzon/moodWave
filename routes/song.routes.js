31cf724e5adf2db034094f99562f0fb851bb5d91
shiraLevinzon <levinzon@g.jct.ac.il>
2023-12-25 11:30:36 +0200

    song routs

---
+++ routes/song.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { auth } = require('../middlewares/auth');
+
+const router = express.Router();
+
+
+router.get("/", );
+router.get("/songById/:id", );
+router.get("/songByName/:name", );
+router.get("/songByEmo/:emotion", );
+router.get("/songByGenre/:genre", );
+router.get("/songByArtist/:artist", );
+router.get("/songByPeriodTag/:tag", );
+
+
+router.post("/", auth(), addTask);
+router.delete("/:id", auth(), deleteSong);
+
