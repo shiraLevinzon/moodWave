be03350af66e034c6d2f5ff88bc4f9778fe5f81e
talyamos <talia.moshe@gmail.com>
2023-12-26 13:49:47 +0200

    route

--- routes/song.routes.js
+++ routes/song.routes.js
@@ -1,23 +1,29 @@
 const express = require("express");
-const { auth } = require('../middlewares/auth');
-const { getAllSongs, getSongById, getSongByName, getSongByArtist, getSongByEmo, getSongByGenre, getSongByPeriodTag ,addSong,deleteSong } = require('../controllers/songController');
+const { auth } = require("../middlewares/auth");
+const {
+  getAllSongs,
+  getSongById,
+  getSongByName,
+  getSongByArtist,
+  getSongByEmo,
+  getSongByGenre,
+  getSongByPeriodTag,
+  addSong,
+  deleteSong,
+} = require("../controllers/songController");
 
 const router = express.Router();
 
+router.get("/", getAllSongs);
+router.get("/songById/:id", getSongById);
+router.get("/songByName", getSongByName);
+router.get("/songByArtist", getSongByArtist);
 
-router.get("/",getAllSongs);
-router.get("/songById/:id",getSongById);
-router.get("/songByName",getSongByName);
-router.get("/songByArtist",getSongByArtist);
-
-router.get("/songByEmo/:emo",getSongByEmo);
-router.get("/songByGenre/:gen",getSongByGenre);
-router.get("/songByPeriodTag/:pTag",getSongByPeriodTag);
-
+router.get("/songByEmo/:emo", getSongByEmo);
+router.get("/songByGenre/:gen", getSongByGenre);
+router.get("/songByPeriodTag/:pTag", getSongByPeriodTag);
 
 router.post("/", auth(), addSong);
 router.delete("/:id", auth(), deleteSong);
 
 module.exports = router;
-
-
