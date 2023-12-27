f60c2fc8aafe8696948615be2b8a03820c51908d
talyamos <talia.moshe@gmail.com>
2023-12-27 13:08:47 +0200

    server

---
+++ server/utils/nodeMailer.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  ///1 create transporter
+  const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+
+    port: 2525,
+    /* secure: false, */
+    auth: {
+      user: "a06ee1664ddd1a",
+      pass: "2c774c9a6a8912",
+    },
+
+    //In gmail use less secure app option
+  });
+  ///2 Define email options
+  const mailOptions = {
+    from: "Monkeys <info@monkeys.com>",
+    to: options.email,
+    subject: "YOUR CODE",
+    text: "AB1234",
+    //html
+  };
+  ///3 send the email
+  await transporter.sendMail(mailOptions);
+};
+module.exports = sendEmail;
